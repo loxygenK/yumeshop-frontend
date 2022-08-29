3aabd884e0082abc1cc467b9f0c454d41f75f14a
loxygenK <me@loxygen.dev>
2022-08-29 13:56:09 +0900

    feat: apply props content to the component

--- src/components/atoms/Tag/index.tsx
+++ src/components/atoms/Tag/index.tsx
@@ -1,9 +1,19 @@
 import { VFC } from 'react';
+import { spacingSizes } from 'src/styles/Tokens';
+import styled from 'styled-components';
 
-export interface TagProps {
+export type TagProps = {
   children: string;
   color: string;
-}
+};
+
+const TagWrapper = styled.li<Pick<TagProps, 'color'>>`
+  display: inline flow-root;
+  background: ${(props) => props.color};
+  list-style: none;
+  padding: 0 ${spacingSizes.xs};
+`;
+
 export const Tag: VFC<TagProps> = ({ children, color }) => (
-  <div>{children}</div>
+  <TagWrapper color={color}>{children}</TagWrapper>
 );
