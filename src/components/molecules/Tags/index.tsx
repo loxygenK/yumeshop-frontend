82e7caa4fec1be0622a8b92fb40159d63dd8d899
loxygenK <me@loxygen.dev>
2022-08-29 15:36:35 +0900

    feat: apply style to Tags component
    Co-authored-by: ten986 <ten986@users.noreply.github.com>

--- src/components/molecules/Tags/index.tsx
+++ src/components/molecules/Tags/index.tsx
@@ -1,13 +1,20 @@
 import { Tag, TagProps } from 'src/components/atoms/Tag';
+import { spacingSizes } from 'src/styles/Tokens';
+import styled from 'styled-components';
+
+const TagsWrapper = styled.ul`
+  display: flex;
+  gap: ${spacingSizes.xxs};
+`;
 
 export type TagsProps = {
   tags: TagProps[];
 };
 
 export const Tags = ({ tags }: TagsProps): JSX.Element => (
-  <ul>
+  <TagsWrapper>
     {tags.map((tag) => (
       <Tag key={tag.name} {...tag} />
     ))}
-  </ul>
+  </TagsWrapper>
 );
