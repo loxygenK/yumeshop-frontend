8127694d8baeb86057266ad9676905e5c7a47561
loxygenK <me@loxygen.dev>
2022-08-30 11:11:47 +0900

    feat: implement design for WideCard

--- src/components/molecules/WideCard/index.tsx
+++ src/components/molecules/WideCard/index.tsx
@@ -1,5 +1,52 @@
 import { TagProps } from 'src/components/atoms/Tag';
+import { breakpoint } from 'src/styles/breakpoint';
+import { fontSizes, roundings, shadows, spacingSizes } from 'src/styles/Tokens';
+import styled, { css } from 'styled-components';
 import { Tags } from '../Tags';
+
+const WideCardWrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  gap: ${spacingSizes.xs};
+
+  box-shadow: ${shadows.md};
+  border-radius: ${roundings.sm};
+
+  ${breakpoint.mb(css`
+    padding: ${spacingSizes.xs};
+  `)}
+  ${breakpoint.tb(css`
+    padding: ${spacingSizes.xs} ${spacingSizes.sm};
+  `)}
+  ${breakpoint.pc(css`
+    padding: ${spacingSizes.sm};
+  `)}
+`;
+
+const MetaInformationLine = styled.div`
+  display: flex;
+  gap: ${spacingSizes.xs};
+`;
+
+const Date = styled.time`
+  font-weight: 700;
+
+  ${breakpoint.mb(css`
+    font-size: ${fontSizes.fontSize14};
+  `)}
+  ${breakpoint.pc(css`
+    font-size: ${fontSizes.fontSize24};
+  `)}
+`;
+
+const Content = styled.p`
+  ${breakpoint.mb(css`
+    font-size: ${fontSizes.fontSize12};
+  `)}
+  ${breakpoint.pc(css`
+    font-size: ${fontSizes.fontSize18};
+  `)}
+`;
 
 export type WideCardProps = {
   date: string;
@@ -7,10 +54,12 @@ export type WideCardProps = {
   text: string;
 };
 export const WideCard = ({ date, tags, text }: WideCardProps): JSX.Element => (
-  <div>
-    {/* TODO: add `datetime` props */}
-    <time>{date}</time>
-    <Tags tags={tags} />
-    <p>{text}</p>
-  </div>
+  <WideCardWrapper>
+    <MetaInformationLine>
+      {/* TODO: add `datetime` props */}
+      <Date>{date}</Date>
+      <Tags tags={tags} />
+    </MetaInformationLine>
+    <Content>{text}</Content>
+  </WideCardWrapper>
 );
