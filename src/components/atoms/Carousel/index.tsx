bcc008d33a14e50852d327620a05fe8f1690eee5
kei Ota <k_ota@yumemi.co.jp>
2022-08-30 17:57:21 +0900

    feat: clientWidth を見るようにした

--- src/components/atoms/Carousel/index.tsx
+++ src/components/atoms/Carousel/index.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 import NextImage from 'next/image';
 import Slider from 'react-slick';
-import { useCarouselElementSize } from 'src/components/atoms/Carousel/useScreenSize';
+// import { useCarouselElementSize } from 'src/components/atoms/Carousel/useScreenSize';
+import { useEffect, useRef, useState } from 'react';
 
 const CarouselWrapper = styled.div<{ height: string }>`
   height: ${(props) => props.height};
@@ -22,16 +23,33 @@ export type CarouselProps = {
   images: CarouselImage[];
 };
 export const Carousel = ({ images }: CarouselProps): JSX.Element => {
-  const rawElementSize = useCarouselElementSize();
-  const elementSize = `${rawElementSize}px`;
+  // const rawElementSize = useCarouselElementSize();
+  // const elementSize = `${rawElementSize - 47}px`;
+
+  const ref = useRef<HTMLDivElement>(null);
+  const [size, setSize] = useState<string>('600px');
+
+  useEffect(() => {
+    const onScreenResize = () => {
+      console.log(ref.current?.clientWidth);
+      console.log(size);
+      setSize(`${Math.min(ref.current?.clientWidth ?? 0, 600)}px`);
+    };
+
+    onScreenResize();
+
+    window.addEventListener('resize', onScreenResize);
+
+    return () => window.removeEventListener('resize', onScreenResize);
+  }, [ref.current?.clientWidth, size, setSize]);
 
   return (
-    <CarouselWrapper height={elementSize}>
-      <Slider centerMode dots infinite variableWidth>
+    <CarouselWrapper ref={ref} height={size}>
+      <Slider centerMode dots infinite variableWidth centerPadding="0px">
         {images.map((image) => (
           <LinkedImage
-            size={elementSize}
-            style={{ width: elementSize }}
+            size={size}
+            style={{ width: size }}
             key={image.id}
             href={image.linkHref}
           >
