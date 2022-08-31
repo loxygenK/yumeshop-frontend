1e3beb7682424a6be1cf374f63adbb972872a1ea
loxygenK <me@loxygen.dev>
2022-08-31 16:43:54 +0900

    feat: initialize dayjs in actual page as well

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import GlobalStyle from 'src/styles/Globals';
+import 'helper/initializeDayjs';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
