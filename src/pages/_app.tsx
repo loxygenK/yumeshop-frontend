ede0fdf4a4cade4d22f422ba31fce6a7cd2cf01e
loxygenK <me@loxygen.dev>
2022-08-31 16:47:25 +0900

    fix: add `src` explicitly

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import GlobalStyle from 'src/styles/Globals';
-import 'helper/initializeDayjs';
+import 'src/helper/initializeDayjs';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
