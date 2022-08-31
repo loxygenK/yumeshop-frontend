f3f7be201f119e7458e9984140d1e4b221de9721
loxygenK <me@loxygen.dev>
2022-08-31 11:33:14 +0900

    feat: create mock provider for SWR

---
+++ src/storybook/decorators/MockAPIProvider.tsx
@@ -0,0 +1,28 @@
+import { ReactNode, useMemo } from 'react';
+import { Middleware, SWRConfig, SWRResponse } from 'swr';
+
+export type MockAPIProviderProps<T> = {
+  children: ReactNode;
+  response: T;
+};
+
+const createMockMiddleWare =
+  <T,>(response: T): Middleware =>
+  () =>
+  (): SWRResponse => ({
+    data: response,
+    mutate: (_) => Promise.resolve<T>(response),
+    isValidating: false,
+  });
+
+export const MockAPIProvider = <T,>({
+  children,
+  response,
+}: MockAPIProviderProps<T>): JSX.Element => {
+  const mockMiddleware = useMemo(
+    () => createMockMiddleWare(response),
+    [response],
+  );
+
+  return <SWRConfig value={{ use: [mockMiddleware] }}>{children}</SWRConfig>;
+};
